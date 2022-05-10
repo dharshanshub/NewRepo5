200fa943898a3ce6ca0ff838d25f09b55fe501cb
Dharshan <dharshankr2000@gmail.com>
2022-05-10 19:41:56 +0530

    vb

--- Medicure_Entity/Patient.cs
+++ Medicure_Entity/Patient.cs
@@ -8,12 +8,14 @@ namespace Medicure_Entity
         [Required(ErrorMessage = "This field must not be empty")]
         public string Name { get; set; }
         [Required(ErrorMessage = "This field must not be empty")]
+        [RegularExpression(@"^\d{10}$", ErrorMessage = "Phone number should contain only 10 digits")]
         [DataType(DataType.PhoneNumber)]
         public string MobileNo { get; set; }
         public string DateOfReg  { get; set; }
         [Required(ErrorMessage = "This field must not be empty")]
         public string Username { get; set; }
         [Required(ErrorMessage = "This field must not be empty")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", ErrorMessage = "password should contain Minimum eight characters, at least one uppercase letter, one lowercase letter and one number")]
 
         [DataType(DataType.Password)]
         public string Password { get; set; }
